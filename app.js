8e00c1287bab7c07eb5991ebc9023b595cd571a2
MEDA <118144136+medaworld@users.noreply.github.com>
2022-11-02 15:54:09 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config({ path: './.env.local' });
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+app.use('/feed', feedRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    app.listen(process.env.PORT || 8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
