db1ad887776c7232815aa74db0a7c47bf378df6b
MEDA <118144136+medaworld@users.noreply.github.com>
2022-11-02 23:32:48 -0700

    Add status fetch and update feats

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const User = require('../models/user');
+
 const authController = require('../controllers/auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -27,5 +29,14 @@ router.put(
 );
 
 router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch(
+  '/status',
+  isAuth,
+  [body('status').trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
