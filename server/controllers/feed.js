5e28198dae5c534cbb4d8bb0bf5aa34fddb47722
MEDA <118144136+medaworld@users.noreply.github.com>
2022-11-02 19:25:15 -0700

    Add delete request

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 const Post = require('../models/post');
+const path = require('path');
+const fs = require('fs');
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -120,6 +122,31 @@ exports.updatePost = (req, res, next) => {
 };
 
 const clearImage = (filePath) => {
+  console.log(filePath);
   filePath = path.join(__dirname, '..', filePath);
   fs.unlink(filePath, (err) => console.log(err));
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 404;
+        throw error;
+      }
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: 'Deleted post' });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
