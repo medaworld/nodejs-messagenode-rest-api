94e9c5058e275997558903441c632316538cb9ba
MEDA <118144136+medaworld@users.noreply.github.com>
2022-11-02 23:01:25 -0700

    Add authorization checks

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -124,6 +124,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized');
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -156,6 +161,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error('Could not find post.');
         error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized');
+        error.statusCode = 403;
         throw error;
       }
       clearImage(post.imageUrl);
