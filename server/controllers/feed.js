3b44c97ab15a3f050582254ad5bfe5f94979bd71
MEDA <118144136+medaworld@users.noreply.github.com>
2022-11-03 21:14:49 -0700

    Fix bugs, update posts, delete posts, and sort correctly

--- server/controllers/feed.js
+++ server/controllers/feed.js
@@ -1,9 +1,11 @@
-const { validationResult } = require('express-validator');
-const Post = require('../models/post');
-const User = require('../models/user');
 const path = require('path');
 const fs = require('fs');
+
+const { validationResult } = require('express-validator');
 const io = require('../socket');
+
+const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -14,6 +16,8 @@ exports.getPosts = (req, res, next) => {
     .then((count) => {
       totalItems = count;
       return Post.find()
+        .populate('creator')
+        .sort({ createdAt: -1 })
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     })
@@ -61,10 +65,13 @@ exports.createPost = (req, res, next) => {
     .then((user) => {
       creator = user;
       user.posts.push(post);
+      io.getIO().emit('posts', {
+        action: 'create',
+        post: { ...post._doc, creator: { _id: req.userId, name: user.name } },
+      });
       return user.save();
     })
     .then((result) => {
-      io.getIO().emit('posts', { action: 'create', post: post });
       res.status(201).json({
         message: 'Post created successfully!',
         post: post,
@@ -120,13 +127,14 @@ exports.updatePost = (req, res, next) => {
     throw error;
   }
   Post.findById(postId)
+    .populate('creator')
     .then((post) => {
       if (!post) {
         const error = new Error('Could not find post.');
         error.statusCode = 404;
         throw error;
       }
-      if (post.creator.toString() !== req.userId) {
+      if (post.creator._id.toString() !== req.userId) {
         const error = new Error('Not authorized');
         error.statusCode = 403;
         throw error;
@@ -140,6 +148,7 @@ exports.updatePost = (req, res, next) => {
       return post.save();
     })
     .then((result) => {
+      io.getIO().emit('posts', { action: 'update', post: result });
       res.status(200).json({ message: 'Post updated!', post: result });
     })
     .catch((err) => {
