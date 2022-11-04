d09b8dc2390e8bcdfbe7a404c03f552842b1dac6
MEDA <118144136+medaworld@users.noreply.github.com>
2022-11-03 19:52:39 -0700

    Set up socket io

--- server/app.js
+++ server/app.js
@@ -61,7 +61,11 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.MONGO_URI)
   .then((result) => {
-    app.listen(process.env.PORT || 8080);
+    const server = app.listen(process.env.PORT || 8080);
+    const io = require('./socket').init(server);
+    io.on('connection', (socket) => {
+      console.log('Client connected');
+    });
   })
   .catch((err) => {
     console.log(err);
